f329bc2be4c9c6e1fa4c1da92097c7f022201736
Paulo Jonas <pjonas21@gmail.com>
2024-01-24 19:52:13 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+import symbolFlash from './assets/pictures/the-flash.png';
+import symbolReverseFlash from './assets/pictures/the-reverse-flash.png';
 
 export default function App() {
+  const [activeState, setActiveState] = useState(false)
+
+  function handleSymbol() {
+    setActiveState((oldValue:boolean) => {
+      return !oldValue
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={activeState ? styles.containerReverse : styles.container}>
+      <TouchableOpacity onPress={handleSymbol}>
+        <Image style={styles.imageCenter} source={activeState ? symbolReverseFlash : symbolFlash} />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#a30512",
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerReverse: {
+    flex: 1,
+    backgroundColor: '#dbc204',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+    backgroundColor: 'black'
+  },
+  imageCenter: {
+    width: 300,
+    height: 400
+  }
 });
